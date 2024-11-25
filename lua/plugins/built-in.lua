a7214c786595043a5ff88067a4b4a4c0469ad80d
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-11-24 17:19:53 -0800

    Add arguments to shfmt

--- lua/plugins/built-in.lua
+++ lua/plugins/built-in.lua
@@ -15,4 +15,16 @@ return {
       }
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        -- # Example of using shfmt with extra args
+        shfmt = {
+          prepend_args = { "-i", "2", "-ci" },
+        },
+      },
+    },
+  },
 }
