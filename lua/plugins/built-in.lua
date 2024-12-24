a1c6ddae19f40d4a9d0ba746a808fd814f729161
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-12-24 15:38:55 -0800

    Remove nvim-cmp configurations
    Now using blink.cmp, the new LazyVim default.

--- lua/plugins/built-in.lua
+++ lua/plugins/built-in.lua
@@ -5,17 +5,6 @@ return {
     opts = { style = "night" },
   },
   {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      }
-    end,
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters = {
