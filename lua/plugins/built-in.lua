f8699bd16960312f89d855bd48e479b9ef829d8a
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-12-26 17:31:14 -0800

    Remove unnecessary conform.nvim configuration

--- lua/plugins/built-in.lua
+++ lua/plugins/built-in.lua
@@ -8,8 +8,6 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters = {
-        injected = { options = { ignore_errors = true } },
-        -- # Example of using shfmt with extra args
         shfmt = {
           prepend_args = { "-i", "2", "-ci" },
         },
