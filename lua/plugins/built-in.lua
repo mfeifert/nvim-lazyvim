5bb886ad1388dbe7c21e63f5c8c8ff4f32908565
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2024-12-31 10:08:13 -0800

    Ensure Tree-sitter parsers are installed automatically

--- lua/plugins/built-in.lua
+++ lua/plugins/built-in.lua
@@ -23,4 +23,26 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
