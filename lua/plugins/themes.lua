8a1d6a026549e2d9da53c703242f347991ba2759
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-01-01 19:05:07 -0800

    Break plugin configurations out into separate files

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,4 +1,9 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
   -- Themes not included in LazyVim by default
   { "miikanissi/modus-themes.nvim" },
   { "ellisonleao/gruvbox.nvim" },
