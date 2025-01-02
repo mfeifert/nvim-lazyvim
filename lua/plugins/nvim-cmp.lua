62c1d9fa5f29e1009c9f132651d1900a0ccbae41
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-01-01 19:26:09 -0800

    Add nvim-cmp configuration

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
+  },
+}
