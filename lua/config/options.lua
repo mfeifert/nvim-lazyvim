32f02f7fc8fe68782ff0a5ea9c3734f2688e59b9
Matthew Feifert <55014056+mfeifert@users.noreply.github.com>
2025-05-19 09:10:40 -0700

    Disable relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@
 
 vim.o.pumblend = 0 -- Popup blend (transparency)
 vim.o.pumheight = 20 -- Maximum number of entries in a popup
+vim.o.relativenumber = false
 vim.o.tabstop = 4
 
 vim.diagnostic.enable(false) -- Disable inline diagnostics at startup
